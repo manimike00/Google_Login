4b030feb19b8e46aff12ee325c8716e5c9fcb561
root <root@localhost.localdomain>
2018-04-01 15:16:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/root/google_login/SecoundOne.java
@@ -0,0 +1,50 @@
+package com.example.root.google_login;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecoundOne extends AppCompatActivity {
+
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secound_one);
+
+        button = (Button) findViewById(R.id.logoutbtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null)
+                {
+                    startActivity(new Intent(SecoundOne.this,MainActivity.class));
+                }
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
+
+    }
+}
